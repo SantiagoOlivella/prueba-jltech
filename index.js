d2f1362621c9ceaabd096af3294589a53b2c8104
SantiagoOlivella <olivellaserna@gmail.com>
2022-01-31 08:09:41 -0500

    feat: conection database and index

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const morgan = require('morgan');
+const cors = require('cors');
+require('./database');
+
+const app = express();
+
+app.set('port', 4000);
+
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(cors({ origin: "*" }));
+
+//Rutas o vistas
+// app.use('/user', require('./routes/User.Route'));
+// app.use('/note', require('./routes/Note.Route'));
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor corriendo en el puertooo', app.get('port'));
+})
